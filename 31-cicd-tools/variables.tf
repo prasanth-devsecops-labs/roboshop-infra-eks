c223417907f5d9585fb4ac94346d214f4e0c96f8
Prasanth Reddy <prasanthreddy1792@gmail.com>
2026-05-01 09:59:52 +0530

    jenkins

---
+++ 31-cicd-tools/variables.tf
@@ -0,0 +1,19 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z082901534H7OC4NT02WB"
+}
+
+variable "domain_name" {
+    default = "prashum.online"
+}
+
+variable "sonar" {
+    default = true
+}
