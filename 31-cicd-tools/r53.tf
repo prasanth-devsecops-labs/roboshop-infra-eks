e5ac57308fcf7cdf7e114992dc1c9a3f03520d1a
Prasanth Reddy <prasanthreddy1792@gmail.com>
2026-05-15 09:23:05 +0530

    runners

--- 31-cicd-tools/r53.tf
+++ 31-cicd-tools/r53.tf
@@ -1,20 +1,20 @@
 resource "aws_route53_record" "jenkins" {
-  count = var.sonar ? 1 : 0
+  count = var.jenkins ? 1 : 0
   zone_id = var.zone_id
   name    = "jenkins.${var.domain_name}"
   type    = "A"
   ttl     = "1"
-  records = [aws_instance.jenkins.public_ip]
+  records = [aws_instance.jenkins[0].public_ip]
   allow_overwrite = true
 }
 
 resource "aws_route53_record" "jenkins_agent" {
-  count = var.sonar ? 1 : 0
+  count = var.jenkins ? 1 : 0
   zone_id = var.zone_id
   name    = "jenkins-agent.${var.domain_name}"
   type    = "A"
   ttl     = "1"
-  records = [aws_instance.jenkins_agent.private_ip]
+  records = [aws_instance.jenkins_agent[0].private_ip]
   allow_overwrite = true
 }
 
